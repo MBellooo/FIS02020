dc7c31b2d419a7b529fc6932626f63581e6b5291
MBllooo <marbello31415@gmail.com>
2025-05-05 12:52:10 -0300

    primeiro commit

---
+++ Fis02020/Fis02020_Funcoes.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 22 16:34:27 2025
+
+@author: mar
+"""
+
---
+++ Fis02020/Teste.py
@@ -0,0 +1,12 @@
+
+from math import sqrt
+from Fis02020_Funcoes import soma
+
+x = 30
+y = 33
+z = 43
+
+print (sqrt(z))
+
+s = soma(x,y,z)
+print (s)
